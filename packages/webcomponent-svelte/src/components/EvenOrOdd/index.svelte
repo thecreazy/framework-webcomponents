036da68d2b042b9ab311ebc4feed8e92b0ff5823
Riccardo Canella <canella_riccardo@icloud.com>
2021-11-16 21:46:30 +0100

    feat(svelte): created base component with all functions

--- packages/webcomponent-svelte/src/components/EvenOrOdd/index.svelte
+++ packages/webcomponent-svelte/src/components/EvenOrOdd/index.svelte
@@ -1,10 +1,12 @@
 <script>
-	export let name;
+	let actualValue = Math.floor(Math.random() * 100000);
+    let whatTypeIs = actualValue % 2 === 0 ? "Even" : "Odd"
 </script>
 
 <main>
 	<h1>Even or odd component in Svelte</h1>
-    <h2>Actual value</h2>
+    <h2>Actual value <b>{actualValue}</b></h2>
+    <h3>The actual value is:  <b>{whatTypeIs}</b></h3>
 </main>
 
 <style>
